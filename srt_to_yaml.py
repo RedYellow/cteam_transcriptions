83650dab4694368607c9f422665616d1bf2fa813
Ryan Mai <ryan.mai@medwatchers.com>
2021-11-29 16:59:56 -0800

    Added theme song and split srt to dict

--- srt_to_yaml.py
+++ srt_to_yaml.py
@@ -1,14 +1,23 @@
 import glob
 import re
+from pathlib import Path
 
-BASE_PATH = "/Users/Nic/Documents/Python Projects/cteam_transcriptions/subtitles"
-paths = glob.glob(BASE_PATH+"/*.srt")
+BASE_PATH = "./subtitles"
+paths = glob.glob(BASE_PATH + "/*.srt")
 
 for path in paths:
-	with open(path) as f:
-		text = f.read()
-	text = re.sub("\n|^\d+\n", "\n", text) # remove the lines containing only digits (a counter I guess)
-	text = re.sub("<.*?>", "", text)
-	breakpoint()
-	with open(Path(path).stem()+".txt", "w") as f:
-		f.write(text)
+    with open(path) as f:
+        text = f.read()
+    text = re.sub(
+        "(\n|^)\d+\n", "\n", text
+    )  # remove the lines containing only digits (a counter I guess)
+    text = re.sub("<.*?>", "", text)
+
+    tstamp_lines = {
+        l.split("\n")[0]: l.split("\n")[1] for l in text.split("\n\n") if len(l) > 1
+    }
+
+    # tstamp_lines =
+    breakpoint()
+    with open(Path("./yaml_files") / Path(path).stem() + ".txt", "w") as f:
+        f.write(text)
