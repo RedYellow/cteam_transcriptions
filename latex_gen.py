9e5df60e0fc44c249671df31ef2565ab96df6720
RedYellow <15852310+RedYellow@users.noreply.github.com>
2022-02-25 10:26:53 -0800

    test file, now rewriting with actual code

--- latex_gen.py
+++ latex_gen.py
@@ -6,34 +6,33 @@ Created on Fri Mar 19 11:09:01 2021
 @author: Nic
 """
 
-from pylatex import Document, Section, Subsection, Command
-from pylatex.utils import italic, NoEscape
-from pylatex.package import Package
-from pylatex import Document, Section, UnsafeCommand
-from pylatex.base_classes import Environment, CommandBase, Arguments
-
 import pdflatex
+from pylatex import Command, Document, Section, Subsection, UnsafeCommand
+from pylatex.base_classes import Arguments, CommandBase, Environment
+from pylatex.package import Package
+from pylatex.utils import NoEscape, italic
 
-def fill_document(doc):
-    """Add a section, a subsection and some text to the document.
+# def fill_document(doc):
+#     """Add a section, a subsection and some text to the document.
 
-    :param doc: the document
-    :type doc: :class:`pylatex.document.Document` instance
-    """
-    doc.packages.append(Package('dramatist'))
+#     :param doc: the document
+#     :type doc: :class:`pylatex.document.Document` instance
+#     """
+#     doc.packages.append(Package("dramatist"))
 
-    with doc.create(Section('A section')):
-        doc.append('Some regular text and some ')
-        doc.append(italic('italic text. '))
+#     with doc.create(Section("A section")):
+#         doc.append("Some regular text and some ")
+#         doc.append(italic("italic text. "))
 
-        with doc.create(Subsection('A subsection')):
-            doc.append('Also some crazy characters: $&#{}')
+#         with doc.create(Subsection("A subsection")):
+#             doc.append("Also some crazy characters: $&#{}")
+#     return doc
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Basic document
-    doc = Document('basic')
-    fill_document(doc)
+    doc = Document("basic")
+    doc = fill_document(doc)
 
-doc.generate_pdf(clean_tex=False, compiler="latexmk")
-doc.generate_tex()
+    doc.generate_pdf(clean_tex=False, compiler="latexmk")
+    doc.generate_tex()
